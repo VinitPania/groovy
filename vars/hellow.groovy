d63034b87b1c910d1252dcd5eeff58bd6c8c3dba
VinitPania <vinit.pania@cloverinfotech.com>
2023-01-16 16:27:09 +0530

    added callApi data_body

--- vars/hellow.groovy
+++ vars/hellow.groovy
@@ -2,3 +2,13 @@ def call(name){
     println "mellow  ${name}, how are you doing today nigga"
 }
 
+def callApi(){
+    def data_Body="""{
+                        "name":"morpheus"
+                        "job":"leader"
+                    }"""
+    
+    
+}
+
+println(callApi())

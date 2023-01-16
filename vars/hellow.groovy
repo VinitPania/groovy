898b9c0462431e542a555b9a88ab264728a6dfc3
VinitPania <vinit.pania@cloverinfotech.com>
2023-01-16 17:01:16 +0530

    removed the call from Api

--- vars/hellow.groovy
+++ vars/hellow.groovy
@@ -2,7 +2,7 @@ def call(name){
     println "mellow  ${name}, how are you doing today nigga"
 }
 
-def callApi(){
+def Api(){
     def data_Body="""{
                         "name":"morpheus"
                         "job":"leader"
